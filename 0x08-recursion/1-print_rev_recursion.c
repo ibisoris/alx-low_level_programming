dcdcf5ec69037959920f8535d20be51bde405748
Ibinabo Orifama <ibisoris2020@gmail.com>
2022-09-29 15:21:27 -0700

    Added print recursion rev file

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
-* _strlen_recursion -> function to get length of string
-* @s: string param
-* Return: length of string
-*/
-int _strlen_recursion(char *s)
-{	
-	if (*s == '\0')
-		return (0);
-	s++;
-	return (1 + _strlen_recursion(s));
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: The string to be printed.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
