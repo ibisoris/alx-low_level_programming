a121fef3c8e525dda45a9b0f8c86bd4b1b34dd64
Ibinabo Orifama <ibisoris2020@gmail.com>
2022-10-10 12:18:57 -0700

    Task 3 file

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,5 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
+#ifndef ABSO
+#define ABSO
 
 /*
  * File: 3-function_like_macro.h
@@ -7,6 +7,6 @@
  *       that computes the absolute value of a number x.
  */
 
-#define ABS(x) ((x) < 0 ? (-x) : (x))
+#define ABS(x) (((x) < (0)) ? (-(x)) : (x))
 
-#endif
+#endif /* ABS(x) */
